aff5fbb3ab0db0730aca68abd9ee20bedfa9a7cd
MasayaMatsushita <masaya.sj.gm@gmail.com>
2025-10-08 10:52:40 +0900

    NextRequestの削除

--- src/app/api/degreeCentrality/route.ts
+++ src/app/api/degreeCentrality/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { calculateDegreeCentrality } from '@/utils/DegreeCentrality';
 
 export async function GET() {
