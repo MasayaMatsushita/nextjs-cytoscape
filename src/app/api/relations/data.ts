14f5467d5391135775bf467af6d3073ec66bedc9
MasayaMatsushita <masaya.sj.gm@gmail.com>
2025-10-09 15:08:05 +0900

    初期データの挿入

--- src/app/api/relations/data.ts
+++ src/app/api/relations/data.ts
@@ -1,4 +1,84 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export const relations: any[] = [
+  {
+    "data": {
+      "id": "r1",
+      "source": "p1",
+      "target": "p4",
+      "department": "教育研究部"
+    }
+  },
+  {
+    "data": {
+      "id": "r2",
+      "source": "p1",
+      "target": "p6",
+      "department": "教育研究部"
+    }
+  },
+  {
+    "data": {
+      "id": "r3",
+      "source": "p1",
+      "target": "p10",
+      "department": "教育研究部"
+    }
+  },
+  {
+    "data": {
+      "id": "r4",
+      "source": "p4",
+      "target": "p6",
+      "department": "教育研究部"
+    }
+  },
+  {
+    "data": {
+      "id": "r5",
+      "source": "p4",
+      "target": "p10",
+      "department": "教育研究部"
+    }
+  },
+  {
+    "data": {
+      "id": "r6",
+      "source": "p6",
+      "target": "p10",
+      "department": "教育研究部"
+    }
+  },
+  {
+    "data": {
+      "id": "r7",
+      "source": "p2",
+      "target": "p5",
+      "department": "情報科学部"
+    }
+  },
+  {
+    "data": {
+      "id": "r8",
+      "source": "p2",
+      "target": "p9",
+      "department": "情報科学部"
+    }
+  },
+  {
+    "data": {
+      "id": "r9",
+      "source": "p5",
+      "target": "p9",
+      "department": "情報科学部"
+    }
+  },
+  {
+    "data": {
+      "id": "r10",
+      "source": "p3",
+      "target": "p7",
+      "department": "技術開発部"
+    }
+  }
 ]
